3176fd837bd74ec380fd7c4b36190b678f792635
marcus <marcus.longoria12@gmail.com>
2020-12-01 17:37:51 -0600

    add http requests to get pokemon data

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,24 @@
+require_relative "../config/environment"
 class Pokedex::API
+    def initialize
+        @url = "https://pokeapi.co/api/v2/pokemon"
+    end
 
+    def get_pokemon_urls
+        uri = URI.parse(@url)
+        response = Net::HTTP.get(uri)
+        data = JSON.parse(response)
+        data["results"].each do |pokemon|
+            get_pokemon_data(pokemon["url"])
+        end
+    end
+
+    def get_pokemon_data(url)
+        uri = URI.parse(url)
+        response = Net::HTTP.get(uri)
+        data = JSON.parse(response)
+        Pokemon.new(data)
+    end
 end
+
+Pokedex::API.new.get_pokemon_urls
