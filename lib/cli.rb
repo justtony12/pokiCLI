3349ad52ecf2a1feed1674da1c0daebe79ecc42d
marcus <marcus.longoria12@gmail.com>
2020-12-02 20:53:06 -0600

    edited CLI to include players name, need to work on input to open urls for each pokemon.

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,20 +9,26 @@ class Pokedex::CLI
        input = ""
 
         while input != "exit"
-            puts "Which Pokemon would you like to learn more about?"
-            puts "Choose a number from 1-20!"
+            puts "To choose a Pokemon to learn more about type 'learn'."
+            sleep(2)
             puts "To close your Pokedex type 'exit'."
 
             input = gets.strip
+
             case input
             when "exit"
             goodbye
+            when "learn"
+                choose_a_pokemon
             end
         end
     end
 
     def greeting
-        puts "Hi! Welcome to your Pokedex: Fetching all pokemon now..."
+        puts "Hi! Welcome to your Pokedex: Please enter your name, trainer."
+        name = gets.strip
+        puts "Hi ya, #{name}!"
+        puts "Fetching your Pokemon now..."
         Pokedex::API.new.get_pokemon_urls
     end
 
@@ -36,8 +42,23 @@ class Pokedex::CLI
         end
     end
 
+    def choose_a_pokemon
+        puts "Which Pokemon would you like to learn about?"
+
+        input = gets.strip.to_i
+    end
+
     def goodbye
         puts "See you soon!"
         exit
+    end
+
+    def choose_a_pokemon
+        puts "Which Pokemon would you like to learn about?"
+
+        input = gets.strip.to_i
+        if (1..Pokedex::Pokemon.all.length).include?(input)
+            puts "add something here..."
+        end
     end
 end
